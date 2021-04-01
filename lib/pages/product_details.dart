c2c1e5bb31a19ce59fdb94795e132a10e78626a8
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-04-01 15:16:53 +0100

    adding product picture, name, price, and buttons to choose size color and quantity and then 3 buttons to buy or add to shopping basket or favorize It

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -52,7 +52,111 @@ class _ProductDetailsState extends State<ProductDetails> {
                 color: Colors.white,
                 child: Image.asset(widget.product_details_picture),
               ),
+              footer: Container(
+                height: 50,
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(
+                    "${widget.product_details_name}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  title: Align(
+                    alignment: Alignment(0, -4),
+                    child: new Row(
+                      children: [
+                        Expanded(
+                            flex: 1,
+                            child: Text(
+                              "${widget.product_details_new_price} MAD",
+                              style: TextStyle(
+                                  fontSize: 15.0,
+                                  color: Colors.red,
+                                  decoration: TextDecoration.none),
+                            )
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Text(
+                              "${widget.product_details_old_price} MAD",
+                              style: TextStyle(
+                                fontSize: 15.0,
+                                color: Colors.grey,
+                                decoration: TextDecoration.lineThrough,
+                              ),
+                            )),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
+          ),
+
+          // Size, color and quantity Buttons
+          Row(
+            children: [
+              Expanded(
+                  child: MaterialButton(
+                    elevation: 0.2,
+                    onPressed: () {},
+                    color: Colors.white,
+                    textColor: Colors.grey,
+                    child: Row(
+                      children: [
+                        Expanded(child: Text("Size")),
+                        Expanded(child: new Icon(Icons.arrow_drop_down)),
+                      ],
+                    ),
+                  ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  elevation: 0.2,
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  child: Row(
+                    children: [
+                      Expanded(child: Text("Color")),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  elevation: 0.2,
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  child: Row(
+                    children: [
+                      //Expanded(child: Text("Quantity",)),
+                      FittedBox(child: Text("Quantity"), fit: BoxFit.cover,),
+                      Expanded(child: new Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  elevation: 0.1,
+                  child: Text("Buy me"),
+                ),
+              ),
+              IconButton(onPressed: (){}, icon: Icon(Icons.add_shopping_cart, color: Colors.red,)),
+              IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border, color: Colors.red,)),
+            ],
           ),
         ],
       ),
