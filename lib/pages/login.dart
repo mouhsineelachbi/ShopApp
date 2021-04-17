4e1a1123c4a271c8fc14666a3761c16b2106e33e
mouhsineelachbi <mouhsineelachbi@gmail.com>
2021-04-17 20:13:58 +0000

    create isSignIn function

---
+++ lib/pages/login.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences preferences;
+  bool Loading = false;
+  bool isLogedin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isSignIn();
+  }
+
+  void isSignIn() async {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+
+
+
+}
